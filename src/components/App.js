7f2913fd6a6e9bbcf25bd10e160d5ceaec76b9d5
rushikeshghuge <rushikeshghuge993@gmail.com>
2023-11-19 07:50:38 +0530

    done!

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  // make s state
+  const [isLoging, setIsLoggin] = useState(false);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+
+      {isLoging ? <h4>You are logged in</h4> :
+
+        <div className="form">
+          <div> UserName : <input type="text" placeholder="Username" /> </div>
+          <div> Password : <input type="text" placeholder="Password" /> </div>
+          <div> <button onClick={() => setIsLoggin(true)}>Submit</button> </div>
+        </div>
+
+      }
+
     </div>
   )
 }
